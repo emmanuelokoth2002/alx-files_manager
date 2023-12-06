29a6f948deae227ead22e38eede6a6bf1f54b1ea
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-12-06 20:44:19 +0300

    Updates

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,35 +1,66 @@
 // controllers/UsersController.js
-const { dbClient } = require('../utils/db');
-const bcrypt = require('bcrypt');
+import sha1 from 'sha1';
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 class UsersController {
-    static async postNew(req, res) {
-        const { email, password } = req.body;
+  /**
+   * Create a new user.
+   * @param {Object} req - Express request object.
+   * @param {Object} res - Express response object.
+   * @returns {Object} - JSON response.
+   */
+  static async postNew(req, res) {
+    const { email, password } = req.body;
 
-        if (!email) {
-            return res.status(400).json({ error: 'Missing email' });
-        }
+    // Check if email and password are provided
+    if (!email) return res.status(400).json({ error: 'Missing email' });
+    if (!password) return res.status(400).json({ error: 'Missing password' });
 
-        if (!password) {
-            return res.status(400).json({ error: 'Missing password' });
-        }
+    // Check if the user with the provided email already exists
+    const existingUser = await dbClient.findUserByEmail(email);
+    if (existingUser) return res.status(400).json({ error: 'User already exists' });
 
-        const existingUser = await dbClient.db().collection('users').findOne({ email });
-        if (existingUser) {
-            return res.status(400).json({ error: 'Already exists' });
-        }
+    // Hash the password
+    const hashedPassword = sha1(password);
 
-        const hashedPassword = await bcrypt.hash(password, 10);
+    // Create a new user in the database
+    const newUser = await dbClient.createUser(email, hashedPassword);
 
-        const newUser = { email, password: hashedPassword };
-        const result = await dbClient.db().collection('users').insertOne(newUser);
+    // Return the newly created user information
+    return res.status(201).json({
+      id: newUser.insertedId,
+      email,
+    });
+  }
 
-        const user = {
-            id: result.insertedId,
-            email,
-        };
-        res.status(201).json(user);
-    }
+  /**
+   * Get information about the authenticated user.
+   * @param {Object} req - Express request object.
+   * @param {Object} res - Express response object.
+   * @returns {Object} - JSON response.
+   */
+  static async getMe(req, res) {
+    const token = req.headers['x-token'];
+
+    // Check if authentication token is provided
+    if (!token) return res.status(401).json({ error: 'Unauthorized' });
+
+    // Retrieve user ID from Redis using the authentication token
+    const userId = await redisClient.get(`auth_${token}`);
+
+    // Check if user ID is found
+    if (!userId) return res.status(401).json({ error: 'Unauthorized' });
+
+    // Retrieve user information from the database using the user ID
+    const user = await dbClient.findUserById(userId);
+
+    // Check if user is found
+    if (!user) return res.status(401).json({ error: 'User not found' });
+
+    // Return user information
+    return res.status(200).json({ id: user._id, email: user.email });
+  }
 }
 
-module.exports = UsersController;
+export default UsersController;
