254f459937d6e78db6f03fb23c1e2106d4cd3acf
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-12-06 20:58:08 +0300

    Updates

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,25 +1,18 @@
 // controllers/AppController.js
-import { Response } from 'express';
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
 class AppController {
-  static async getStatus(_, res) {
-    const status = {
-      redis: redisClient.isAlive(),
-      db: dbClient.isAlive(),
-    };
-    res.status(200).json(status);
+  static getStatus(req, res) {
+    const redisAlive = redisClient.isAlive();
+    const dbAlive = dbClient.isAlive();
+    res.status(200).json({ redis: redisAlive, db: dbAlive });
   }
 
-  static async getStats(_, res) {
-    const usersCount = await dbClient.nbUsers();
-    const filesCount = await dbClient.nbFiles();
-    const stats = {
-      users: usersCount,
-      files: filesCount,
-    };
-    res.status(200).json(stats);
+  static async getStats(req, res) {
+    const nbusers = await dbClient.nbUsers();
+    const nbfiles = await dbClient.nbFiles();
+    res.status(200).json({ users: nbusers, files: nbfiles });
   }
 }
 
--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,56 +1,74 @@
-// controllers/AuthController.js
-import { Response, Request } from 'express';
 import { v4 as uuidv4 } from 'uuid';
 import sha1 from 'sha1';
-import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 class AuthController {
-  static async getConnect(req: Request, res: Response) {
-    const authHeader = req.header('Authorization');
-    
+  /**
+   * Authenticate user and generate a token.
+   * @param {Object} req - Express request object.
+   * @param {Object} res - Express response object.
+   * @returns {Object} - JSON response.
+   */
+  static async getConnect(req, res) {
+    // Check if Authorization header is present
+    const authHeader = req.headers.authorization;
     if (!authHeader || !authHeader.startsWith('Basic ')) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
 
+    // Decode and extract credentials from the Authorization header
     const base64Credentials = authHeader.split(' ')[1];
-    const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
+    const credentials = Buffer.from(base64Credentials, 'base64').toString('utf-8');
     const [email, password] = credentials.split(':');
-
     if (!email || !password) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
 
-    // Find user in DB
-    const hashedPassword = sha1(password);
-    const user = await dbClient
-      .client
-      .db(dbClient.database)
-      .collection('users')
-      .findOne({ email, password: hashedPassword });
-
+    // Find user by email in the database
+    const user = await dbClient.findUserByEmail(email);
     if (!user) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
 
-    // Generate token and store in Redis
-    const token = uuidv4();
-    const key = `auth_${token}`;
-    await redisClient.set(key, user._id.toString(), 86400); // 24 hours
-    return res.status(200).json({ token });
-  }
-
-  static async getDisconnect(req: Request, res: Response) {
-    const { 'x-token': xToken } = req.headers;
-
-    if (!xToken) {
+    // Verify the hashed password
+    const hashedPassword = sha1(password);
+    if (hashedPassword !== user.password) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
 
-    // Delete token in Redis
-    const key = `auth_${xToken}`;
-    await redisClient.del(key);
-    
+    // Generate a random token and store it in Redis for 24 hours
+    const randomToken = uuidv4();
+    const key = `auth_${randomToken}`;
+    await redisClient.set(key, user._id.toString(), 86400);
+
+    // Return the generated token
+    return res.status(200).json({ token: randomToken });
+  }
+
+  /**
+   * Disconnect user by deleting the token from Redis.
+   * @param {Object} req - Express request object.
+   * @param {Object} res - Express response object.
+   * @returns {Object} - JSON response.
+   */
+  static async getDisconnect(req, res) {
+    // Check if X-Token header is present
+    const token = req.headers['x-token'];
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    // Retrieve user ID from Redis using the token
+    const userId = await redisClient.get(`auth_${token}`);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    // Delete the token from Redis
+    await redisClient.del(`auth_${token}`);
+
+    // Return a successful response with no content
     return res.status(204).end();
   }
 }
